4c74761d3dc2dcb9f0b11806a4042062636a385c
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-11-16 09:07:52 +0200

    create, get, decline, and accept ticket are done

--- src/components/barber/tickets/RequestTickets.jsx
+++ src/components/barber/tickets/RequestTickets.jsx
@@ -11,6 +11,7 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
 import zIndex from '@material-ui/core/styles/zIndex';
 import { getQueuesAction } from '../../../store/actions';
+import styles from '../styles/requests.module.css';
 
 const StyledMenu = withStyles({
   paper: {
@@ -58,15 +59,17 @@ export default function RequestTickets() {
   const dispatch = useDispatch();
   let barberId = useSelector((state) => state?.authReducer?.user?.id);
   let client_id = useSelector((state) => state?.authReducer?.user?.id);
+
+  // fetch tickets
   async function fetchTickets() {
+    console.log('barberId', barberId);
     const response = await instance.get(`/barber/requests/${barberId}`);
-    console.log('responsessssssssss', response.data);
+    console.log('responsessssssssss all tickets', response.data);
     setAllTickets(response.data);
   }
   // did mount
   useEffect(() => {
     fetchTickets();
-    console.log('barberId', barberId);
   }, []);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -86,86 +89,96 @@ export default function RequestTickets() {
   async function addToQueue(ticket) {
     // { barbarId, serviseId, clientId, time }
     let ticketData = {
-      barbarId: barberId,
+      barbarId: ticket.barber_id,
       serviseId: ticket.service_id,
-      clientId: client_id,
+      clientId: ticket.client_id,
       time: ticket.time,
       id: ticket.id,
     };
     const response = await instance.post(`/barber/queue/post`, ticketData);
+    // deleteTicket(ticket.id);
     fetchTickets();
+
     // dispatch(getQueuesAction(!queueState));
   }
 
   return (
-    <div style={{ position: 'relative', zIndex: '51551313515' }}>
-      {!allTickets.length ? (
-        <NotificationsIcon
-          aria-controls='customized-menu'
-          variant='contained'
-          aria-haspopup='true'
-          onClick={handleClick}
-          style={{
-            color: '#f1f1f1',
-            fontSize: 29,
-            position: 'absolute',
-            top: '-22px',
-          }}
-        />
-      ) : (
-        <NotificationsActiveIcon
-          variant='contained'
-          aria-controls='customized-menu'
-          aria-haspopup='true'
-          onClick={handleClick}
-          style={{
-            color: 'red',
-            fontSize: 29,
-            position: 'absolute',
-            top: '-22px',
-          }}
-        />
-      )}
-      {/* <NotificationsNoneIcon aria-controls='customized-menu' aria-haspopup='true' variant='contained' onClick={handleClick} /> */}
+    <>
+      <div style={{ position: 'relative', zIndex: '51551313515' }}>
+        {!allTickets.length ? (
+          <NotificationsIcon
+            aria-controls='customized-menu'
+            variant='contained'
+            aria-haspopup='true'
+            onClick={handleClick}
+            style={{
+              color: '#f1f1f1',
+              fontSize: 29,
+              position: 'absolute',
+              top: '-22px',
+            }}
+          />
+        ) : (
+          <>
+            <h6 style={{ position: 'absolute', top: '-70px', right: '-35px', color: 'red' }}>{allTickets.length}</h6>
+            <NotificationsActiveIcon
+              variant='contained'
+              aria-controls='customized-menu'
+              aria-haspopup='true'
+              onClick={handleClick}
+              style={{
+                color: '#f1f1f1',
+                fontSize: 29,
+                position: 'absolute',
+                top: '-22px',
+              }}
+            />
+          </>
+        )}
+        {/* <NotificationsNoneIcon aria-controls='customized-menu' aria-haspopup='true' variant='contained' onClick={handleClick} /> */}
 
-      <StyledMenu id='customized-menu' anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose} style={{ paddingTop: '50px' }}>
-        {allTickets?.map((ticket) => {
-          return (
-            <StyledMenuItem key={ticket.id} style={{ display: 'block' }}>
-              <ListItemIcon
-                style={{
-                  // marginTop:'1rem',
-                  width: '35rem',
-                  height: '5rem',
-                  display: 'flex',
-                  flexWrap: 'wrap',
-                  justifyContent: 'space-evenly',
-                  alignItems: 'center',
-                }}
-              >
-                <img
-                  src={url + ticket.profile_pic}
+        <StyledMenu id='customized-menu' anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose} style={{ paddingTop: '50px' }}>
+          {allTickets?.map((ticket) => {
+            return (
+              <StyledMenuItem key={ticket.id} style={{ display: 'block', margin: 'auto' }}>
+                <ListItemIcon
                   style={{
-                    width: '5rem',
+                    // marginTop:'1rem',
+                    width: '35rem',
                     height: '5rem',
-                    marginButton: '3rem',
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
                   }}
-                  fontSize='small'
-                />
-                <h5>{ticket.user_name}</h5>
-                <h5>{ticket.service_name}</h5>
+                >
+                  <img
+                    src={url + ticket.profile_pic}
+                    style={{
+                      width: '5rem',
+                      height: '5rem',
+                      marginButton: '3rem',
+                    }}
+                    fontSize='small'
+                  />
+                  <h5>{ticket.user_name}</h5>
+                  <h5>{ticket.service_name}</h5>
 
-                <h5>{ticket.estimated_time} min</h5>
+                  <h5>{ticket.estimated_time} min</h5>
 
-                <h5>{ticket.time}</h5>
-
-                <CheckCircleOutlineIcon onClick={() => addToQueue(ticket)} />
-                <HighlightOffIcon onClick={() => deleteTicket(ticket.id)} />
-              </ListItemIcon>
-            </StyledMenuItem>
-          );
-        })}
-      </StyledMenu>
-    </div>
+                  <h5>{ticket.time}</h5>
+                  <span className={styles.accept}>
+                    <CheckCircleOutlineIcon onClick={() => addToQueue(ticket)} />
+                  </span>
+                  <span className={styles.decline}>
+                    <HighlightOffIcon onClick={() => deleteTicket(ticket.id)} />
+                  </span>
+                </ListItemIcon>
+              </StyledMenuItem>
+            );
+          })}
+        </StyledMenu>
+      </div>
+    </>
   );
 }
